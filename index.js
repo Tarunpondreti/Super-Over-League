3ccdeb6e07f16ed7762cc2a36d787743acb97321
tarun <tarunpondreti@example.com>
2022-11-01 08:49:57 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="container">
+    <h1 className="greeting">Super over league</h1>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image greeting"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image greeting"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
